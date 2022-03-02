fcfc96be28b82c851e28e1b0b960f3615f9bb120
enurberk <elifnurber@hotmail.com>
2022-03-02 15:29:27 +0300

    setting up server, routes, model, mongoDB

---
+++ Config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.PORT,
+    mogoURI: process.env.MONGO_URI,
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const keys = require('./Config/keys.js');
+const app = express();
+
+//setting up DB
+const mongoose = require('mongoose');
+mongoose.connect(keys.mogoURI);
+
+//setup DB models
+require('./model/Account');
+
+//setup the routes
+require('./routes/authenticationRoutes')(app);
+
+app.listen(keys.port, () => {
+    console.log("Listining on " + keys.port);
+});
